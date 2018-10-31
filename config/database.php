938c4234a6c0ce9dc0300bada3fbf493ab30208b
himakeerthi <hg256@njit.edu>
2018-10-31 16:32:34 -0400

    Fix : Fixed the postgres database to work on heroku

--- config/database.php
+++ config/database.php
@@ -1,4 +1,7 @@
 <?php
+$heroku_db_url = parse_url(env('DATABASE_URL', "postgres://forge:forge@localhost:5432/forge"));
+
+
 
 return [
 
@@ -13,24 +16,23 @@ return [
     |
     */
 
-    $heroku_db_url = parse_url(env('DATABASE_URL', "postgres://forge:forge@localhost:5432/forge"));
+    'default' => env('DB_CONNECTION', 'mysql'),
 
-
-/*
-|--------------------------------------------------------------------------
-| Database Connections
-|--------------------------------------------------------------------------
-|
-| Here are each of the database connections setup for your application.
-| Of course, examples of configuring each database platform that is
-| supported by Laravel is shown below to make development simple.
-|
-|
-| All database work in Laravel is done through the PHP PDO facilities
-| so make sure you have the driver for your particular database of
-| choice installed on your machine before you begin development.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the database connections setup for your application.
+    | Of course, examples of configuring each database platform that is
+    | supported by Laravel is shown below to make development simple.
+    |
+    |
+    | All database work in Laravel is done through the PHP PDO facilities
+    | so make sure you have the driver for your particular database of
+    | choice installed on your machine before you begin development.
+    |
+    */
 
     'connections' => [
 
@@ -82,21 +84,18 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+        'pg-heroku' => [
+            'driver' => 'pgsql',
+            'host' => $heroku_db_url['host'],
+            'database' => substr($heroku_db_url['path'], 1),
+            'username' => $heroku_db_url['user'],
+            'password' => $heroku_db_url['pass'],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+        ],
 
-    'pg-heroku' => [
-        'driver' => 'pgsql',
-        'host' => $heroku_db_url['host'],
-        'database' => substr($heroku_db_url['path'], 1),
-        'username' => $heroku_db_url['user'],
-        'password' => $heroku_db_url['pass'],
-        'charset' => 'utf8',
-        'prefix' => '',
-        'schema' => 'public',
     ],
-
-
-
-],
 
     /*
     |--------------------------------------------------------------------------
