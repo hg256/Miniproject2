250c093bccdf12e93a5bd1ecd521a279ba30bd63
himakeerthi <hg256@njit.edu>
2018-11-01 02:05:56 -0400

    Fix : fixed the register and login page

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -17,7 +17,7 @@
             <a class="nav-link" href="{{route('login')}}">Login</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="{{route('login')}}">Register</a>
+            <a class="nav-link" href="{{route('register')}}">Register</a>
         </li>
     </ul>
 </div>
